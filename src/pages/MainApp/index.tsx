9b616a0023c06432324621ba7d97f2bea6597e50
Luiz Rogerio <lrbengozi@gmail.com>
2021-05-29 08:40:34 -0300

    Create a folder structure

---
+++ src/pages/MainApp/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const MainApp: React.FC = () => {
+  return (
+    <View>
+      <Text>Home Page</Text>
+    </View>
+  );
+};
+
+export default MainApp;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const SignIn: React.FC = () => {
+  return (
+    <View>
+      <Text>Tela de Login</Text>
+    </View>
+  );
+};
+
+export default SignIn;
