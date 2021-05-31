8e759732caed812f5a82687c60b36ca668d3c63e
Luiz Rogerio <lrbengozi@gmail.com>
2021-05-31 16:54:10 -0300

    Create login page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,69 @@
 import React from 'react';
 
-import {View, Text} from 'react-native';
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  boxCenter: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    padding: 10,
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: '#e46400',
+  },
+  loginForm: {
+    padding: 10,
+  },
+  input: {
+    borderWidth: 0.5,
+    borderRadius: 5,
+    padding: 10,
+    margin: 2.5,
+  },
+  loginButton: {
+    alignItems: 'center',
+    margin: 2.5,
+    padding: 10,
+    backgroundColor: '#e46400',
+    borderRadius: 5,
+  },
+});
 
 const SignIn: React.FC = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const doLogin = () => {};
+
   return (
-    <View>
-      <Text>Login Page</Text>
+    <View style={styles.boxCenter}>
+      <Text style={styles.title}>LoginAPP</Text>
+      <View style={styles.loginForm}>
+        <TextInput
+          style={styles.input}
+          value={email}
+          placeholder="E-mail"
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          placeholder="Password"
+          onChangeText={setPassword}
+        />
+        <TouchableOpacity style={styles.loginButton} onPress={doLogin}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
